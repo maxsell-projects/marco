c32af5a0187c4e7f073407374d4b803af4c13f22
Gxlvxao <elenaofteste@gmail.com>
2026-01-21 16:35:47 -0300

    meta tag add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="YIkBVcqcJlSgkBE-Hk-UayMhulG3cQncMwK1B0qHYI4" />
     
     <title>@yield('title', 'Porthouse | Private Office')</title>
     
